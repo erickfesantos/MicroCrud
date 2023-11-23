9ea665045cb3e62201310428206c21e05efb18ad
erickfesantos <143459790+erickfesantos@users.noreply.github.com>
2023-11-23 10:41:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,83 @@
-let parte_2 = document.querySelector('#parte2'); 
-let opcoes = [...document.querySelectorAll('input[name="opcao"]')];
-let botao_nome_selecionado = document.querySelector('#Nselecionado');
-let botao_remover_selecionado = document.querySelector('#Rselecionado');
-let botao_adicionar_elemento = document.querySelector('#adicionar');
-let texto = document.querySelector('#texto');
+const parte_2 = document.querySelector('#parte2'); 
+const caixas = [...document.querySelectorAll('.opcoes')];
+const botao_nome_selecionado = document.querySelector('#Nselecionado');
+const botao_remover_selecionado = document.querySelector('#Rselecionado');
+const botao_adicionar_elemento = document.querySelector('#adicionar');
+const botao_adicionar_elemento_antes = document.querySelector('#adicionar_antes');
+const botao_adicionar_elemento_depois = document.querySelector('#adicionar_depois');
+const texto = document.querySelector('#texto');
 
-function input_selecionado(){
-  let selecionado_array = [...document.querySelectorAll('input[name="opcao"]:checked')];
+function caixa_selecionada(){
+  let selecionado_array = [...document.querySelectorAll('.selecao_vermelha')];
   return selecionado_array[0];
 }
 
+function remover_selecao(){
+  let caixas_para_remover = [...document.querySelectorAll('.opcoes')];
+  caixas_para_remover.map((elemento)=>{
+    elemento.classList.remove("selecao_vermelha");
+  })
+}
+
+function criar_div(){
+    let div = document.createElement("div")
+    div.setAttribute("class", "opcoes");
+    let paragrafo = document.createElement("p")
+    div.appendChild(paragrafo);
+    div.addEventListener("click", ()=>{
+      remover_selecao();
+      div.classList.toggle("selecao_vermelha");
+    });
+    paragrafo.textContent = texto.value;
+    return div;
+}
+
+caixas.map((elemento)=>{
+  elemento.addEventListener("click", ()=>{
+    remover_selecao();
+    elemento.classList.toggle("selecao_vermelha");
+  });
+});
+
+
 botao_nome_selecionado.addEventListener("click", ()=>{
-  let selecionado = input_selecionado();
-  alert('O nome selecionado foi: ' + selecionado.parentNode.firstChild.textContent);
+  let selecionado = caixa_selecionada();
+  alert('O nome selecionado foi: ' + selecionado.textContent);
 });
 
 botao_remover_selecionado.addEventListener("click", ()=>{
-  let selecionado = input_selecionado();
-  selecionado.parentNode.remove();
+  let selecionado = caixa_selecionada();
+  selecionado.remove();
 });
 
 
 botao_adicionar_elemento.addEventListener("click", ()=>{
-  //criar restrição para só conseguir ADICIONAR se tiver texto no input
- // Criar função para essa parte do Código
-  let div = document.createElement("div")
-  div.setAttribute("class", "opcoes");
-  let paragrafo = document.createElement("p")
-  let input = document.createElement("input")
-  input.setAttribute("type", "radio");
-  input.setAttribute("name", "opcao");
-  div.appendChild(paragrafo);
-  div.appendChild(input);
-  paragrafo.textContent = texto.value;
-  parte_2.appendChild(div);
-  texto.value = '';
+  if(texto.value != ''){
+    parte_2.appendChild(criar_div());
+    texto.value = '';
+  }else{
+    alert("Digite o nome da nova caixa no campo abaixo");
+  }
 });
 
+botao_adicionar_elemento_antes.addEventListener("click", ()=>{
+    if(texto.value != ''){
+      let selecionado = caixa_selecionada();
+      parte_2.insertBefore(criar_div(), selecionado);
+      texto.value = '';
+    }else{
+      alert("Digite o nome da nova caixa no campo abaixo");
+  }
+});
+
+botao_adicionar_elemento_depois.addEventListener("click", ()=>{
+    if(texto.value != ''){
+      let selecionado = caixa_selecionada();
+      parte_2.insertBefore(criar_div(), selecionado.nextSibling);
+      texto.value = '';
+    }else{
+      alert("Digite o nome da nova caixa no campo abaixo");
+    }
+});
 
 
